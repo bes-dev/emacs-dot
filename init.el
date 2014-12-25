4f8710f60c724beec27e00c3db18a21ce3d4cc82
sergei <belbes122@yandex.ru>
2014-12-25 13:34:04 +0300

    update init.el

--- init.el
+++ init.el
@@ -43,13 +43,14 @@
 	       '(
 		 dired-details
 		 window-numbering
-		 sr-speedbar
 		 revive
-		 google-translate
 		 cmake-mode
-		 cedit
-		 ecb
 		 auto-complete-clang
+		 yasnippet
+		 auto-complete
+		 auto-complete-c-headers
+		 magit
+         multiple-cursors
 		 ))
  '("package" "packages" "install"))
 
@@ -63,12 +64,28 @@
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 
-;; ------------------------------------------------------------
-;; sr-speedbar
-;; ------------------------------------------------------------
-(require 'sr-speedbar)
-(custom-set-variables '(speedbar-show-unknown-files t))
-(global-set-key (kbd "<f12>") 'sr-speedbar-toggle)
+(require 'ibuffer nil t)
+
+;; ibuffer groups
+(setq-default ibuffer-saved-filter-groups
+			  (quote (("default"
+					   ("org"  (mode . org-mode))
+					   ("dired" (mode . dired-mode))
+					   ("D" (mode . d-mode))
+					   ("C/C++" (or
+								 (mode . cc-mode)
+								 (mode . c-mode)
+								 (mode . c++-mode)))
+					   ("magit" (name . "^\\*magit"))
+					   ("Markdown" (mode . markdown-mode))
+					   ("emacs" (name . "^\\*Messages\\*$"))
+					   ("shell commands" (name . "^\\*.*Shell Command\\*"))))))
+
+(add-hook 'ibuffer-mode-hook
+		  (lambda ()
+			(ibuffer-switch-to-saved-filter-groups "default")))
+
+(global-set-key (kbd "\C-x \C-b") 'ibuffer)
 
 ;; ------------------------------------------------------------
 ;; Save backup files into specialize directory
@@ -94,7 +111,7 @@
        (add-hook 'dired-mode-hook
 		 '(lambda ()
 		    (dired-details-install)
-		    (setq dired-details-hidden-string "--- ")
+		    (setq dired-details-nhidden-string "--- ")
 		    (define-key dired-mode-map (kbd "h") 'dired-details-toggle))))))
 
 (eval-after-load "window-numbering-autoloads"
@@ -140,52 +157,113 @@
        (define-key ctl-x-map "R" 'revive-restore-window-configuration)
        (revive-restore-window-configuration))))
 
-;; google translate
-(require 'google-translate)
-(require 'google-translate-smooth-ui)
-(global-set-key "\C-ct" 'google-translate-smooth-translate)
+;; ------------------------------------------------------------
+;; magit
+;; ------------------------------------------------------------
+(require 'magit)
 
+
+;; ------------------------------------------------------------
+;; org-mode
+;; ------------------------------------------------------------
+(add-hook 'org-mode-hook (lambda ()
+                           (toggle-truncate-lines -1)))
+
+;; ------------------------------------------------------------
+;; multiple-cursor
+;; ------------------------------------------------------------
+(eval-after-load "multiple-cursors-autoloads"
+  '(progn
+     (when (require 'multiple-cursors nil t)
+       (defun mc/mark-all-dispatch ()
+         "- add a fake cursor at current position
+
+- call mc/edit-lines if multiple lines are marked
+
+- call mc/mark-all-like-this if marked region is on a single line"
+         (interactive)
+         (cond
+          ((not (region-active-p))
+           (mc/create-fake-cursor-at-point)
+           (mc/maybe-multiple-cursors-mode))
+          ((> (- (line-number-at-pos (region-end))
+                 (line-number-at-pos (region-beginning))) 0)
+           (mc/edit-lines))
+          (t
+           (mc/mark-all-like-this))))
+
+       (defun mc/align ()
+         "Aligns all the cursor vertically."
+         (interactive)
+         (let ((max-column 0)
+               (cursors-column '()))
+           (mc/for-each-cursor-ordered
+            (mc/save-excursion
+             (goto-char (overlay-start cursor))
+             (let ((cur (current-column)))
+               (setq cursors-column (append cursors-column (list cur)))
+               (setq max-column (if (< max-column cur) cur max-column)))))
+
+           (defun mc--align-insert-times ()
+             (interactive)
+             (dotimes (_ times)
+               (insert " ")))
+
+           (mc/for-each-cursor-ordered
+            (let ((times (- max-column (car cursors-column))))
+              (mc/execute-command-for-fake-cursor 'mc--align-insert-times cursor))
+            (setq cursors-column (cdr cursors-column)))))
+
+       (setq mc/list-file "~/.mc-lists.el")
+       (load mc/list-file t) ;; load, but no errors if it does not exist yet please
+
+       (global-set-key (kbd "C->")  'mc/mark-next-like-this)
+       (global-set-key (kbd "C-<")  'mc/mark-previous-like-this)
+
+       (global-set-key (kbd "M-@") 'mc/mark-all-dispatch)
+       (global-set-key (kbd "M-#") 'mc/insert-numbers)
+       (global-set-key (kbd "M-'") 'mc/align))))
 
 ;; ------------------------------------------------------------
 ;; IDE for C/C++
 ;; ------------------------------------------------------------
 
 (require 'cc-mode)
+(global-set-key (kbd "C-x C-i") 'linum-mode)
 
 (setq-default c-basic-offset 4 c-default-style "linux")
 (setq-default tab-width 4 indent-tabs-mode t)
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
 
-;;; activate ecb
-(require 'ecb)
-(require 'ecb-autoloads)
+(defun my-c++-mode-hook ()
+  (setq c-basic-offset 4)
+  (c-set-offset 'substatement-open 0))
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
-;;; configure ecb
-(custom-set-variables '(ecb-options-version "2.40"))
-(custom-set-faces)
-(setq ecb-examples-bufferinfo-buffer-name nil)
-(setq ecb-tip-of-the-day nil)
-;;; window style
-(setq ecb-layout-name "left3")
-(setq ecb-show-sources-in-directories-buffer 'always)
-(setq ecb-compile-window-height 12)
-;;; key bindings
-;;; activate and deactivate ecb
-(global-set-key (kbd "C-x C-;") 'ecb-activate)
-(global-set-key (kbd "C-x C-'") 'ecb-deactivate)
-;;; show/hide ecb window
-(global-set-key (kbd "C-;") 'ecb-show-ecb-windows)
-(global-set-key (kbd "C-'") 'ecb-hide-ecb-windows)
-;;; quick navigation between ecb windows
-(global-set-key (kbd "C-)") 'ecb-goto-window-edit1)
-(global-set-key (kbd "C-!") 'ecb-goto-window-directories)
-(global-set-key (kbd "C-@") 'ecb-goto-window-sources)
-(global-set-key (kbd "C-#") 'ecb-goto-window-methods)
-(global-set-key (kbd "C-$") 'ecb-goto-window-compilation)
+; start auto-complete with emacs
+(require 'auto-complete)
+; do default config for auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
 
-;; auto-complete
-(require 'auto-complete-clang)
-(define-key c++-mode-map (kbd "C-M-<return>") 'ac-complete-clang) ;; replace C-S-<return> with a key binding that you want
+(c-add-style "my" '("gnu"
+                    (c-offsets-alist . ((innamespace . [0])))))
+
+(add-hook 'c++-mode-hook (lambda ()
+                           (c-set-style "my")))
+
+;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(defun my:ac-c-headers-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  (add-to-list 'achead:include-directories '"/usr/lib/gcc/x86_64-linux-gnu/4.8/include")
+)
+
+(add-hook 'c++-mode-hook 'my:ac-c-headers-init)
+(add-hook 'c-mode-hook 'my:ac-c-headers-init)
 
 ; Add cmake listfile names to the mode list.
 (require 'cmake-mode)
@@ -236,6 +314,17 @@ also (>=23.4)"
 		(cd (add-sudo-to-filename (expand-file-name default-directory))))
 	  (shell-command (read-from-minibuffer "ctags command: " ctags-command)))
 	(visit-tags-table tags-file-name)))
+
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq-default line-number-mode t)
+(setq-default column-number-mode t)
+(fset 'yes-or-no-p 'y-or-n-p)
+;; navigation between buffers
+(global-set-key "\C-x\C-p" 'previous-buffer)
+(global-set-key "\C-x\C-n" 'next-buffer)
+(global-set-key "\C-x\C-\\" 'other-window)
 
 (global-set-key "\C-x\C-u"          'update-tags-file)
 (global-set-key "\C-x\C-v"          'visit-tags-table)
