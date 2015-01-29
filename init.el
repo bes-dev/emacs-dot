4dd8cad85dc1fde61671465c6aa3e37dd42672c0
sergei <belbes122@yandex.ru>
2015-01-30 01:28:10 +0300

    add kill-all-dired-buffers

--- init.el
+++ init.el
@@ -235,3 +235,16 @@
 (setq linum-format " %d") ;; задаем формат нумерации строк
 
 (global-set-key (kbd "C-x t") 'term)
+
+(defun kill-all-dired-buffers ()
+      "Kill all dired buffers."
+      (interactive)
+      (save-excursion
+        (let ((count 0))
+          (dolist (buffer (buffer-list))
+            (set-buffer buffer)
+            (when (equal major-mode 'dired-mode)
+              (setq count (1+ count))
+              (kill-buffer buffer)))
+          (message "Killed %i dired buffer(s)." count))))
+
