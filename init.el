25e61efffd5763ae8a55668a33c8c9db43f26f88
sergei <belbes122@yandex.ru>
2015-03-02 17:04:06 +0300

    added coment or uncoment region or line

--- init.el
+++ init.el
@@ -294,3 +294,14 @@
                   (replace-match "" nil nil))))))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+
+(global-set-key (kbd "C-?") 'comment-or-uncomment-region-or-line)
